a09232270d0b08b2e5cc3dd0e45efa7b3406f240
Maurice Roach <moeroach94@gmail.com>
2018-07-11 17:33:07 -0400

    Cut out the middleman

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import LoginFormContainer from './session/login_form_container';
-import SignupFormContainer from './session/signup_form_container';
+import LoginFormContainer from './session/login_form';
+import SignupFormContainer from './session/signup_form';
 
 const App = () => (
   <div>
--- frontend/components/session/signup_form_container.js
+++ frontend/components/session/signup_form_container.js
@@ -1,7 +1,7 @@
 // import { connect } from 'react-redux';
 import SignupForm from './signup_form';
 import { signup } from '../../actions/session_actions';
-import React from 'react';
+// import React from 'react';
 
 const mapStateToProps = state => {
   return {
