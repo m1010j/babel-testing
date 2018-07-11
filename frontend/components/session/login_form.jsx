45c3529e992b36977ff2e8f0c7a087b1f4b88134
Maurice Roach <moeroach94@gmail.com>
2018-07-11 18:08:50 -0400

    fadfadsf

--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -2,43 +2,47 @@ import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 
-class Thing extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     username: "",
-  //     password: ""
-  //   };
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  // }
+// class Thing extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //     username: "",
+//   //     password: ""
+//   //   };
+//   //   this.handleSubmit = this.handleSubmit.bind(this);
+//   // }
+//
+//   // handleSubmit(e) {
+//   //   e.preventDefault();
+//   //   this.props.login(this.state);
+//   // }
+//   //
+//   // update(field) {
+//   //   return e => this.setState({
+//   //     [field]: e.currentTarget.value
+//   //   });
+//   // }
+//
+//   // renderErrors() {
+//   //   return (
+//   //     <ul>
+//   //       {this.props.errors.map((error, idx) => (
+//   //         <li key={`error-${idx}`}>{error}</li>
+//   //       ))}
+//   //     </ul>
+//   //   );
+//   // }
+//
+//
+//   render() {
+//     return (
+//       <Link to="/your_mum">we in here</Link>
+//     );
+//   }
+// }
 
-  // handleSubmit(e) {
-  //   e.preventDefault();
-  //   this.props.login(this.state);
-  // }
-  //
-  // update(field) {
-  //   return e => this.setState({
-  //     [field]: e.currentTarget.value
-  //   });
-  // }
-
-  // renderErrors() {
-  //   return (
-  //     <ul>
-  //       {this.props.errors.map((error, idx) => (
-  //         <li key={`error-${idx}`}>{error}</li>
-  //       ))}
-  //     </ul>
-  //   );
-  // }
-
-
-  render() {
-    return (
-      <Link to="/your_mum">we in here</Link>
-    );
-  }
-}
-
-export default withRouter(Thing);
+export default withRouter(function Thing() {
+  return (
+    <Link to="/your_mum">we in here</Link>
+  )
+});
