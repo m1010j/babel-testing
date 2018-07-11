8202bbdfc595eba7b8800d851811b66be7e26639
Maurice Roach <moeroach94@gmail.com>
2018-07-11 17:15:40 -0400

    Put router back in

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 // import LoginFormContainer from './session/login_form_container';
 // import SignupFormContainer from './session/signup_form_container';
 
 const App = () => (
   <div>
     <h1>IKE! by Bogdan</h1>
-    {/*<Route path="/login" component={LoginFormContainer} />
-  <Route path="/signup" component={SignupFormContainer} />*/}
+    <Route path="/login" render={props => <h1>login</h1>} />
+  <Route path="/signup" render={props => <h2>signup</h2>} />
   </div>
 );
 
