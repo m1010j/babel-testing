a2e2dd368102ec1e17c1b9b44c99f402d84616a6
Maurice Roach <moeroach94@gmail.com>
2018-07-11 18:00:34 -0400

    fadfadsf

--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -2,68 +2,43 @@ import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 
-// class LoginForm extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       username: "",
-//       password: ""
-//     };
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-//
-//   handleSubmit(e) {
-//     e.preventDefault();
-//     this.props.login(this.state);
-//   }
-//
-//   update(field) {
-//     return e => this.setState({
-//       [field]: e.currentTarget.value
-//     });
-//   }
-//
-//   renderErrors() {
-//     return (
-//       <ul>
-//         {this.props.errors.map((error, idx) => (
-//           <li key={`error-${idx}`}>{error}</li>
-//         ))}
-//       </ul>
-//     );
-//   }
-//
-//
-//   render() {
-//     return (
-//       <div>
-//         Please Login
-//         <form onSubmit={this.handleSubmit}>
-//
-//           <label>Username:
-//             <input type="text" value={this.state.username}
-//               onChange={this.update("username")}
-//             />
-//           </label>
-//
-//           <label>Password:
-//             <input type="password" value={this.state.password}
-//               onChange={this.update("password")}
-//             />
-//           </label>
-//
-//           <input type="submit" value="Log In" />
-//         </form>
-//
-//         <Link to="/signup">New to Ike!? Sign up</Link>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default withRouter(LoginForm);
-export default withRouter(function Thing() {
-  return (
-    <Link to="/your_mum">we in here</Link>
-  );
-});
+class Thing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.login(this.state);
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  renderErrors() {
+    return (
+      <ul>
+        {this.props.errors.map((error, idx) => (
+          <li key={`error-${idx}`}>{error}</li>
+        ))}
+      </ul>
+    );
+  }
+
+
+  render() {
+    return (
+      <Link to="/your_mum">we in here</Link>
+    );
+  }
+}
+
+export default withRouter(Thing);
