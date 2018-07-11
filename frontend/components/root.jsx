c32cefcd8461975337fd0a52afe07e0d40cbc456
Maurice Roach <moeroach94@gmail.com>
2018-07-11 17:01:06 -0400

    No more provider

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
 
 const Root = ({ store }) => (
-  <Provider store={ store }>
     <HashRouter>
-      <h1>Testing</h1>
+      <App/>
     </HashRouter>
-  </Provider>
 );
 
 export default Root;
