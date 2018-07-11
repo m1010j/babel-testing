c2942243de02f565a6678bb7fd1487f1acc9493a
Maurice Roach <moeroach94@gmail.com>
2018-07-11 17:07:29 -0400

    No more router

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+// import { HashRouter } from 'react-router-dom';
 import App from './app';
 
 const Root = ({ store }) => (
-    <HashRouter>
+    <Provider store={store}>
       <App/>
-    </HashRouter>
+    </Provider>
 );
 
 export default Root;
